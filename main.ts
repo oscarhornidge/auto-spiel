de8d8c059eab41c9a4ca6948a487daf5c39be315
oscarhornidge <79571606+oscarhornidge@users.noreply.github.com>
2021-04-21 10:32:06 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,74 +20,79 @@ basic.forever(function () {
     basic.pause(randint(500, 12000))
     Auto_0 = game.createSprite(0, 0)
     Speed_0 = randint(500, 1500)
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(Speed_0)
         Auto_0.change(LedSpriteProperty.Y, 1)
     }
     if (sprite.isTouching(Auto_0)) {
-        basic.showIcon(IconNames.No)
-        basic.pause(2000)
         game.gameOver()
     }
     Auto_0.delete()
+    if (game.isRunning()) {
+        game.setScore(game.score() + 1)
+    }
 })
 basic.forever(function () {
     basic.pause(randint(500, 8000))
     Auto_1 = game.createSprite(1, 0)
     Speed_1 = randint(500, 1500)
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(Speed_1)
         Auto_1.change(LedSpriteProperty.Y, 1)
     }
     if (sprite.isTouching(Auto_1)) {
-        basic.showIcon(IconNames.No)
-        basic.pause(2000)
         game.gameOver()
     }
     Auto_1.delete()
+    if (game.isRunning()) {
+        game.setScore(game.score() + 1)
+    }
 })
 basic.forever(function () {
     basic.pause(randint(500, 5000))
     Auto_2 = game.createSprite(2, 0)
     Speed_2 = randint(500, 1500)
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(Speed_2)
         Auto_2.change(LedSpriteProperty.Y, 1)
     }
     if (sprite.isTouching(Auto_2)) {
-        basic.showIcon(IconNames.No)
-        basic.pause(2000)
         game.gameOver()
     }
     Auto_2.delete()
+    if (game.isRunning()) {
+        game.setScore(game.score() + 1)
+    }
 })
 basic.forever(function () {
     basic.pause(randint(500, 3000))
     Auto_3 = game.createSprite(3, 0)
     Speed_3 = randint(500, 1500)
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(Speed_3)
         Auto_3.change(LedSpriteProperty.Y, 1)
     }
     if (sprite.isTouching(Auto_3)) {
-        basic.showIcon(IconNames.No)
-        basic.pause(2000)
         game.gameOver()
     }
     Auto_3.delete()
+    if (game.isRunning()) {
+        game.setScore(game.score() + 1)
+    }
 })
 basic.forever(function () {
     basic.pause(randint(500, 10000))
     Auto_4 = game.createSprite(4, 0)
     Speed_4 = randint(500, 1500)
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 5; index++) {
         basic.pause(Speed_4)
         Auto_4.change(LedSpriteProperty.Y, 1)
     }
     if (sprite.isTouching(Auto_4)) {
-        basic.showIcon(IconNames.No)
-        basic.pause(2000)
         game.gameOver()
     }
     Auto_4.delete()
+    if (game.isRunning()) {
+        game.setScore(game.score() + 1)
+    }
 })
